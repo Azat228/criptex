3c0e961b426e7fe439165876e346312474840ae4
Azat <47598184+Azat228@users.noreply.github.com>
2025-08-01 15:18:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const img = ['bear','fox','lion','pig','cow','dog','shark'];
+let squere = document.querySelectorAll('.square');
+console.log(squere);
+
+function random(min,max){
+    return Math.round(min-0.5 + Math.random()*(max-min+1));
+}
+
+squere.forEach(function(element) {
+    let randNum = random(0,img.length-1);
+    console.log(randNum);
+    element.style.backgroundImage = 'url(image/' + img[randNum] + '.png)';
+    element.dataset.data =randNum;
+    element.onclick = changeImage;
+});
+ function changeImage(){
+     document.querySelector('#out').innerHTML = '';
+    let data = this.dataset.data;
+    console.log(data);
+    if (+data+1<img.length) {
+        data++;
+    }else{
+        data = 0;
+    }
+    this.dataset.data = data;
+    this.style.backgroundImage = 'url(image/' + img[data] + '.png)';
+    checkSurprise();
+}
+
+function checkSurprise( ){
+    let res = [];
+    squere.forEach(function (element) {
+        res.push(element.dataset.data);
+    });
+    if (res.join('')=='0000') {
+        document.querySelector('#out').src = 'image/bear.png';
+    }else if (res.join('')=='1111') {
+        document.querySelector('#out').src = 'image/fox.png';
+    }else if (res.join('')=='2222') {
+        document.querySelector('#out').src = 'image/lion.png';
+    }else if (res.join('')=='3333') {
+        document.querySelector('#out').src = 'image/pig.png';
+    }else if (res.join('')=='4444') {
+        document.querySelector('#out').src = 'image/cow.png';
+    }else if (res.join('')=='5555') {
+        document.querySelector('#out').src = 'image/dog.png';
+    }else if (res.join('')=='6666') {
+        document.querySelector('#out').src = 'image/shark.png';
+    }
+}
